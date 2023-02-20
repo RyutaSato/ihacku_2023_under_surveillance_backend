575ed71b08b14368277a05d36ef208bc677e3be3
rsato <sato.ryuta.main@gmail.com>
2023-02-21 05:44:11 +0900

    :sparkle: added websocket route

--- app.py
+++ app.py
@@ -1,12 +1,39 @@
-from flask import Flask
+import cv2
+import numpy as np
+from flask import Flask, request
+from flask_socketio import SocketIO, send
+from face_detector import show_detected_face
 
 app = Flask(__name__)
+socketio = SocketIO(app)
+namespace = "/ws"
+
+@socketio.on('connect', namespace)
+def on_connect():
+    print('WebSocket connected')
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+@socketio.on('disconnect', namespace)
+def on_disconnect():
+    print('WebSocket disconnected')
+
+@socketio.on('message', namespace)
+def on_stream(msg: str):
+    data = bytes(msg)
+    # EOIマーカーの位置を検索
+    end: int = data.find(b'\xff\xd9')
+
+    # EOIマーカーが見つかった場合
+    if end != -1:
+        # JPEG画像のバイナリデータを取得
+        frame_data = data[:end + 2]
+
+        # バイナリデータから画像を読み込む
+        img = cv2.imdecode(np.frombuffer(frame_data, dtype=np.uint8), cv2.IMREAD_GRAYSCALE)
+        if show_detected_face(img, is_gray=True):
+            socketio.send({"face": True}, json=True)
+
 
 
 if __name__ == '__main__':
-    app.run()
+    socketio.run(app)
--- face_detector/camera.py
+++ face_detector/camera.py
@@ -7,20 +7,23 @@ face_cascade = cv2.CascadeClassifier(face_cascade_path)  # 顔検出の分類器
 eye_cascade = cv2.CascadeClassifier(eye_cascade_path)  # 目検出の分類器
 
 
-def show_detected_face(img):
+def show_detected_face(img, is_gray=False):
     # 画像をグレースケールに変換（学習済みのモデルがグレースケールでの検出前提だから）
-    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    if not is_gray:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     # faces: 検出した部分の座標を返すイテレーター
-    faces = face_cascade.detectMultiScale(img_gray, scaleFactor=1.1, minNeighbors=5)
+    faces = face_cascade.detectMultiScale(img, scaleFactor=1.1, minNeighbors=5)
     for x, y, w, h in faces:
         # 画面上に描画
         cv2.rectangle(img=img, pt1=(x, y), pt2=(x + w, y + h), color=(255, 0, 0), thickness=2)
         face = img[y: y + h, x: x + w]
-        face_gray = img_gray[y: y + h, x: x + w]
+        face_gray = img[y: y + h, x: x + w]
         eyes = eye_cascade.detectMultiScale(face_gray)
         for (ex, ey, ew, eh) in eyes:
             cv2.rectangle(face, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
             print(ex, ey, ew, eh)
+        return True
+    return False
 
 
 def face_detect_from_webcam():
